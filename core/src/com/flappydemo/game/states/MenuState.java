30717b99430886f50c80bbcb4f38025b749c05bf
vorobev <alex18v@mail.ru>
2017-05-16 15:01:35 +0300

    Initial commit

---
+++ core/src/com/flappydemo/game/states/MenuState.java
@@ -0,0 +1,44 @@
+package com.flappydemo.game.states;
+
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.flappydemo.game.FlappyDemo;
+
+public class MenuState extends State{
+
+    private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT); // отрисовываем фон
+        sb.draw(playBtn, (FlappyDemo.WIDTH / 2) - (playBtn.getWidth() / 2), FlappyDemo.HEIGHT /2);
+        sb.end();
+
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
+
+    }
+}
